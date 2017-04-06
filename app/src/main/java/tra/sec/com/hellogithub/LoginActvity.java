360897de4d6565e235e8e40115120134b60aadd1
AnhTra <trabk1992@gmail.com>
2017-04-06 18:21:24 +0700

    Add LoginActivity

---
+++ app/src/main/java/tra/sec/com/hellogithub/LoginActvity.java
@@ -0,0 +1,10 @@
+package tra.sec.com.hellogithub;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Admin on 4/6/2017.
+ */
+
+public class LoginActvity extends AppCompatActivity{
+}
